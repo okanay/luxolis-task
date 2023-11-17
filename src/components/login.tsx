7ef7c539901f702b210bfe00a5bdc70a6fd4b7f4
okanay <okanay@hotmail.com>
2023-11-17 20:51:10 +0300

    Form Mobile Padding Add

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,7 +4,7 @@ import { LoginImage } from '@/components/login-image';
 export const Login = () => {
   return (
     <div className={'h-screen w-full bg-primary-400'}>
-      <div className={'mx-auto flex h-full w-full max-w-sm flex-col items-center justify-center px-2'}>
+      <div className={'mx-auto flex h-full w-full max-w-sm flex-col items-center justify-center px-8 smTablet:px-2'}>
         <LoginImage />
         <LoginForm />
       </div>
